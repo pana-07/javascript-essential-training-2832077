f927914094add33e43ed8d17744a148969b05122
pana-07 <PANASHEMACHEKANO71@GMAIL.COM>
2025-09-15 10:56:30 +0200

    Update kettle.js

--- Practice/03_12/kettle.js
+++ Practice/03_12/kettle.js
@@ -1,3 +1,29 @@
+class Kettle {
+  constructor(name, power, color, waterLevel, isOn) {
+    this.name = name;
+    this.power = power;
+    this.color = color;
+    this.waterLevel = waterLevel;
+    this.isOn = isOn;
+  }
+
+  addWater(amount) {
+    this.waterLevel += amount;
+    console.log(`Added ${amount}ml water. Current level: ${this.waterLevel}ml`);
+  }
+
+  turnOn() {
+    if (this.waterLevel > 0) {
+      this.isOn = true;
+      console.log(`${this.name} is now ON.`);
+    } else {
+      console.log("Cannot turn on: No water!");
+    }
+  }
+}
+
+export default Kettle;
+
 import Kettle from "./kettle.js";
 
 const myKettle = new Kettle("Whistling Kettle", 1500, "red", 0, false);
